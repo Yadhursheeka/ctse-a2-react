f0b6a18dde353b01b467e0f8d861d93d10329a51
arosh-segar <arosh.segar@gmail.com>
2022-05-18 20:48:19 +0530

    api integrated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [login, setLogin] = useState("Not Logged In");
+  const [products, setProducts] = useState("Products Not retrieved");
+  const [payment, setPayment] = useState("Not Paid");
+
+  const getLogin = async () => {
+    const response = await axios.get("http://34.133.230.64");
+
+    setLogin(response.data.Login);
+  };
+
+  const getProducts = async () => {
+    const response = await axios.get("http://34.69.145.8:8080");
+    setProducts(response.data);
+  };
+
+  const getPayment = async () => {
+    const response = await axios.get("http://34.70.220.201:8080/api/service");
+
+    setPayment(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from docker!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        <button onClick={getLogin}>Login</button>
+        <p>{login}</p>
+      </div>
+      <div>
+        <button onClick={getProducts}>Get Products</button>
+        <p>{products}</p>
+      </div>
+      <div>
+        <button onClick={getPayment}>Pay</button>
+        <p>{payment}</p>
+      </div>
     </div>
   );
 }
